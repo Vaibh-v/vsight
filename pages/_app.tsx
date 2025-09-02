15563f1205a551d7c4b9966704e8dcfcb9e1d395
Vaibh-v <165192898+Vaibh-v@users.noreply.github.com>
2025-09-02 19:25:17 +0530

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,2 +1,19 @@
-import type { AppProps } from "next/app";import { SessionProvider } from "next-auth/react";import "../styles/globals.css";
-export default function MyApp({ Component, pageProps }: AppProps){const { session, ...rest } = (pageProps as any);return(<SessionProvider session={session}><Component {...rest} /></SessionProvider>);}
+import type { AppProps } from "next/app";
+import { SessionProvider } from "next-auth/react";
+import "../styles/globals.css";
+import { SWRConfig } from "swr";
+import { AppStateProvider } from "../components/state/AppStateProvider";
+
+export default function MyApp({ Component, pageProps }: AppProps) {
+  // @ts-ignore – next-auth adds session on pageProps
+  const { session, ...rest } = pageProps;
+  return (
+    <SessionProvider session={session}>
+      <SWRConfig value={{ fetcher: (u: string) => fetch(u).then(r => r.json()), revalidateOnFocus: false, dedupingInterval: 60_000 }}>
+        <AppStateProvider>
+          <Component {...rest} />
+        </AppStateProvider>
+      </SWRConfig>
+    </SessionProvider>
+  );
+}
