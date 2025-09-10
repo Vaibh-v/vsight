a0a5c3062f2e7945e680cc836a4d19c13f878602
Vaibh-v <165192898+Vaibh-v@users.noreply.github.com>
2025-09-10 20:15:38 +0530

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,16 +1,13 @@
 import type { AppProps } from "next/app";
 import { SessionProvider } from "next-auth/react";
-import "../styles/globals.css";
 import { AppStateProvider } from "../components/state/AppStateProvider";
-import Layout from "../components/Layout";
+import "../styles/globals.css";
 
-export default function MyApp({ Component, pageProps }: AppProps & { pageProps: any }) {
+export default function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <SessionProvider session={pageProps.session}>
+    <SessionProvider session={(pageProps as any).session}>
       <AppStateProvider>
-        <Layout>
-          <Component {...pageProps} />
-        </Layout>
+        <Component {...pageProps} />
       </AppStateProvider>
     </SessionProvider>
   );
