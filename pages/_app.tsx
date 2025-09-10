3019b9608dd00f0e69ef4e585c146d10ae0f3597
Vaibh-v <165192898+Vaibh-v@users.noreply.github.com>
2025-09-10 21:17:45 +0530

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,33 @@
+// pages/_app.tsx
 import type { AppProps } from "next/app";
 import { SessionProvider } from "next-auth/react";
-import { AppStateProvider } from "../components/state/AppStateProvider";
-import "../styles/globals.css";
+import { SWRConfig } from "swr";
+import { AppStateProvider } from "@/components/state/AppStateProvider";
+import Layout from "@/components/Layout";
+import "@/styles/globals.css";
 
-export default function MyApp({ Component, pageProps }: AppProps) {
+const fetcher = async (url: string) => {
+  const r = await fetch(url);
+  if (!r.ok) {
+    let msg = `Request failed: ${r.status}`;
+    try {
+      const j = await r.json();
+      if (j?.error) msg = j.error;
+    } catch {}
+    throw new Error(msg);
+  }
+  return r.json();
+};
+
+export default function MyApp({ Component, pageProps: { session, ...pageProps } }: AppProps) {
   return (
-    <SessionProvider session={(pageProps as any).session}>
+    <SessionProvider session={session}>
       <AppStateProvider>
-        <Component {...pageProps} />
+        <SWRConfig value={{ fetcher, revalidateOnFocus: false }}>
+          <Layout>
+            <Component {...pageProps} />
+          </Layout>
+        </SWRConfig>
       </AppStateProvider>
     </SessionProvider>
   );
