f5de8329eb3cf3d98515b10ca63ac2727f9227a5
Vaibh-v <165192898+Vaibh-v@users.noreply.github.com>
2025-09-22 02:42:09 +0530

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,2 +1,6 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = { content: ["./pages/**/*.{ts,tsx}", "./components/**/*.{ts,tsx}"], theme: { extend: {} }, plugins: [] };
+module.exports = {
+  content: ["./pages/**/*.{ts,tsx}", "./components/**/*.{ts,tsx}"],
+  theme: { extend: {} },
+  plugins: []
+};
